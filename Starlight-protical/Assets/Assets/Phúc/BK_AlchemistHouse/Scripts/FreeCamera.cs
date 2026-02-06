2020924bb4d6415f478555597a4166a0def63210
nhphuc760 <dkmtaoko@gmail.com>
2026-02-07 02:05:35 +0700

    modify asset

--- Starlight-protical/Assets/Assets/Phúc/BK_AlchemistHouse/Scripts/FreeCamera.cs
+++ Starlight-protical/Assets/Assets/Phúc/BK_AlchemistHouse/Scripts/FreeCamera.cs
@@ -3,10 +3,38 @@
 public class FreeCamera : MonoBehaviour{
 
 	public float movementSpeed = 5.0f;
+	bool isLocked = true;
 
-	void FixedUpdate()
+    public bool IsLocked { get => isLocked;
+		set {
+			if (value)
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+			else
+			{
+				Cursor.lockState = CursorLockMode.None;				
+			}
+			isLocked = value;
+		} }
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			IsLocked = !isLocked;
+		}
+    }
+
+    void FixedUpdate()
 	{
-		var horizontalAxis = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * movementSpeed;
+		if (!isLocked) return;
+		var horizontalAxis = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * movementSpeed ;
 		var verticalAxis = Input.GetAxis("Vertical") * Time.fixedDeltaTime * movementSpeed;
 		var lookX = Input.GetAxis ("Mouse X");
 		var lookY = Input.GetAxis ("Mouse Y");
