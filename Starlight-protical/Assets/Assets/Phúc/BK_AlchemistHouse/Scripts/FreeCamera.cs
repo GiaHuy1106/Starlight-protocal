d0583b65b9978b3f5be2ea565664148b146e07b3
nhphuc760 <dkmtaoko@gmail.com>
2026-02-08 00:27:39 +0700

    Create Map

--- Starlight-protical/Assets/Assets/Phúc/BK_AlchemistHouse/Scripts/FreeCamera.cs
+++ Starlight-protical/Assets/Assets/Phúc/BK_AlchemistHouse/Scripts/FreeCamera.cs
@@ -1,39 +1,19 @@
 ﻿using UnityEngine;
 
-public class FreeCamera : MonoBehaviour{
+public class FreeCamera : MonoBehaviour
+{
 
 	public float movementSpeed = 5.0f;
-	bool isLocked = true;
-
-    public bool IsLocked { get => isLocked;
-		set {
-			if (value)
-			{
-				Cursor.lockState = CursorLockMode.Locked;
-			}
-			else
-			{
-				Cursor.lockState = CursorLockMode.None;				
-			}
-			isLocked = value;
-		} }
+	
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    private void Update()
-    {
-		if (Input.GetKeyDown(KeyCode.LeftControl))
-		{
-			IsLocked = !isLocked;
-		}
-    }
+   
 
     void FixedUpdate()
-	{
-		if (!isLocked) return;
+	{	
 		var horizontalAxis = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * movementSpeed ;
 		var verticalAxis = Input.GetAxis("Vertical") * Time.fixedDeltaTime * movementSpeed;
 		var lookX = Input.GetAxis ("Mouse X");
---
+++ Starlight-protical/Assets/Scripts/Phuc_Dev/Player.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float gravity = -19.2f;
+    [SerializeField] float jumpHeight = 3f;
+    bool isGrounded;
+    bool canMove;
+    Vector2 Velocity;
+    Vector2 moveInput;  
+
+    private void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+        }
+
+
+
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Velocity.y += gravity * Time.fixedDeltaTime;
+        controller.Move(Velocity * Time.fixedDeltaTime);
+    }
+
+
+    void Movement()
+    {
+        if (!canMove) return;
+        Vector3 Move = (transform.right * moveInput.x + transform.forward * moveInput.y) * Time.fixedDeltaTime * moveSpeed;
+        controller.Move(Move);
+    }
+
+
+    public void LockMove(bool value)
+    {
+        canMove = value;
+    }
+}
